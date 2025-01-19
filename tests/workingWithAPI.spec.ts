2cede410494fec9743b385cc0313f6e9ba2defa0
Lee Porter <leeporter@live.co.uk>
2025-01-19 11:45:21 +0000

    more changes

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import tags from '../test-data/tags.json'
 
 test.beforeEach(async({page}) => {
@@ -9,25 +9,57 @@ test.beforeEach(async({page}) => {
   })
 })
 
+
+  await page.goto('https://conduit.bondaracademy.com')
+  await page.waitForTimeout(1000)
+  await page.getByText('Sign in').click()
+  await page.getByRole('textbox', {name: "Email"}).fill('lptestuser@test.com')
+  await page.getByRole('textbox', {name: "Password"}).fill('password1')
+  await page.getByRole('button').click()
+
+})
+
+test('has title', async ({ page }) => {
   await page.route('*/**/api/articles*', async route => {
     const response = await route.fetch()
     const responseBody = await response.json()
-    responseBody.articles[0].title = "This is a test title"
-    responseBody.articles[0].description = "This is a description"
+    responseBody.articles[0].title = "This is a MOCK test title"
+    responseBody.articles[0].description = "This is a MOCK description"
 
     await route.fulfill({
       body:JSON.stringify(responseBody)
     })
   })
 
-
-  await page.goto('https://conduit.bondaracademy.com')
-  // wait is needed before the script is so short that the mock doesnt have time to react before the page loads
   await page.waitForTimeout(1000)
-})
-
-test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
-  await expect(page.locator('app-article-list p').first()).toContainText('This is a description')
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
 });
+
+test('delete article', async({page, request}) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      "user":{"email":"lptestuser@test.com","password":"password1"}
+    }
+  })
+  const responseBody = await response.json()
+  console.log(responseBody.user.token)
+  const accessToken = responseBody.user.token
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data:{
+      "article":{"tagList":[], "title":"This is a Mock test title", "description":"This is a mock description", "body":"This is a test body"}
+    },
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  })
+  expect(articleResponse.status()).toEqual(201)
+
+  await page.getByText('Global Feed').click()
+  await page.getByText('This is a Mock test title').click()
+  await page.getByRole('button', {name: "Delete Article"}).first().click()
+  await page.getByText('Global Feed').click()
+
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a MOCK test title')
+})
