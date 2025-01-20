e4a25b724561807611e2b2de4cff0970c6284b54
Lee Porter <leeporter@live.co.uk>
2025-01-20 13:56:57 +0000

    Adding auth setup steps to run before every test so the session is shared and tiem saved.

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -3,6 +3,7 @@ import tags from '../test-data/tags.json'
 
 test.beforeEach(async({page}) => {
   // The "*/**" allows us to have a wildcard, so the only thing we care about is the actual endpoint requests at the end.
+  
   await page.route('*/**/api/tags', async route => {
     await route.fulfill({
       body: JSON.stringify(tags)
@@ -11,12 +12,7 @@ test.beforeEach(async({page}) => {
 
 
   await page.goto('https://conduit.bondaracademy.com')
-  await page.waitForTimeout(1000)
-  await page.getByText('Sign in').click()
-  await page.getByRole('textbox', {name: "Email"}).fill('lptestuser@test.com')
-  await page.getByRole('textbox', {name: "Password"}).fill('password1')
-  await page.getByRole('button').click()
-
+  await page.waitForTimeout(2000)
 })
 
 test('has title', async ({ page }) => {
@@ -31,7 +27,7 @@ test('has title', async ({ page }) => {
     })
   })
 
-  await page.waitForTimeout(1000)
+  await page.getByText('Global Feed').click()
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
   await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
   await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
@@ -62,4 +58,36 @@ test('delete article', async({page, request}) => {
   await page.getByText('Global Feed').click()
 
   await expect(page.locator('app-article-list h1').first()).not.toContainText('This is a MOCK test title')
+})
+
+test('create article', async({page, request}) => {
+  await page.getByText('New Article').click()
+  await page.getByRole('textbox', {name:'Article Title'}).fill('Playwright is awesome')
+  await page.getByRole('textbox', {name: 'What\'s this article about?'}).fill('About the Playwright')
+  await page.getByRole('textbox', {name: 'Write your article (in markdown)'}).fill('We like to use playwright for automation')
+  await page.getByRole('button', {name:'Publish Article'}).click()
+  const articleResponse = await page.waitForResponse('https://conduit-api.bondaracademy.com/api/articles/')
+  const articleResponseBody = await articleResponse.json()
+  const slugId = articleResponseBody.article.slug
+
+  await expect(page.locator('.article-page h1')).toContainText('Playwright is awesome')
+  await page.getByText('Home').click()
+  await page.getByText('Global Feed').click()
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('Playwright is awesome')
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data:{
+      "user":{"email":"lptestuser@test.com","password":"password1"}
+    }
+  })
+  const responseBody = await response.json()
+  const accessToken = responseBody.user.token
+  
+  const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    headers: {
+      Authorization: `Token ${accessToken}`
+    }
+  })
+  expect(deleteArticleResponse.status()).toEqual(204)
 })
